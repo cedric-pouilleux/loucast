1c411a9e869456b25a696e198fc665c4a60892c9
Minart <mino.mnz@gmail.com>
2022-11-30 20:11:42 +0100

    Add netlify extendConfig for nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    experimental: {
+        reactivityTransform: true,
+        viteNode: false
+    },
+    hooks: {
+        'vite:extendConfig'(config, { isServer }) {
+            if (isServer) {
+                // Workaround for netlify issue
+                // https://github.com/nuxt/framework/issues/6204
+                // @ts-ignore
+                config.build.rollupOptions.output.inlineDynamicImports = true
+            }
+        }
+    },
 })
