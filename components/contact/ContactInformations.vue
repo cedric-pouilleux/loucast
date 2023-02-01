1f5a1246bce0e5dc7fa3bede0665c3fb399def88
Minart <mino.mnz@gmail.com>
2023-02-01 12:52:56 +0100

    Add informations list, start contact form

---
+++ components/contact/ContactInformations.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="informations">
+    <h3>Informations</h3>
+    <ul>
+      <li>
+        <Icon
+          class="list-icon"
+          :size="iconSize"
+          name="mdi:email"
+        />contact@loucast.fr
+      </li>
+      <li>
+        <Icon
+          :size="iconSize"
+          class="list-icon"
+          name="entypo-social:instagram"
+        /> Instagram
+      </li>
+      <li>
+        <Icon
+          :size="iconSize"
+          class="list-icon"
+          name="entypo-social:soundcloud"
+        /> Soundcloud
+      </li>
+      <li>
+        <Icon
+          :size="iconSize"
+          class="list-icon"
+          name="entypo-social:facebook"
+        /> Facebook
+      </li>
+      <li>
+        <Icon
+          :size="iconSize"
+          class="list-icon"
+          name="mdi:phone"
+        />0712589563 <span class="schedule"> (9h - 18h)</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const iconSize = '12px'
+</script>
+
+<style scoped lang="scss">
+.informations {
+  ul {
+    li {
+      line-height: 40px;
+      font-size: 1.4em;
+      display: flex;
+      align-items: center;
+
+      .schedule {
+        font-size: .7em;
+        margin: 0 12px;
+        color: #555555;
+      }
+
+      .list-icon {
+        margin-right: 10px;
+      }
+    }
+  }
+}
+</style>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,7 +1,39 @@
 <template>
   <NuxtLayout>
-    <div class="contact-page" />
+    <div class="contact-page">
+      <h2>Contact</h2>
+      <section class="contact-page__wrapper">
+        <contact-informations />
+        <contact-form />
+      </section>
+    </div>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts"></script>
+
+<script>
+import ContactForm from '../../components/contact/ContactForm'
+export default {
+  components: { ContactForm }
+}
+</script>
+<style scoped lang="scss">
+.contact-page {
+  h2 {
+    font-size: 2em;
+    color: #333;
+    margin: 20px;
+    text-align: center;
+  }
+  &__wrapper {
+    display: flex;
+    font-family: 'Space Grotesk', sans-serif;
+    justify-content: space-evenly;
+
+    > div {
+      padding: 0 20px;
+    }
+  }
+}
+</style>
